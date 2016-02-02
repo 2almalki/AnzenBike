cc4ef9749e5be8405af27abdc4d6b294bd751725
2almalki <aalmalki22@gmail.com>
2016-02-02 14:54:38 -0500

    adding auto run v1

---
+++ autoStartup/anzenbike_startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# ensure the script is placed in /ect/rc.local
+# paste this line: /yourpath/bin/anzenbike_startup.sh &
+
+sleep 10
+# sudo python /path/for/ultrasonic &
+# sudo python /path/for/camera &
+# sudo python /path/for/motorCamera &
+# sudo ./pathforLidar &
+# sudo python /path/for/GPIOintegration &
