5ab4e3caea5cd09db911308b35378e74d681bbfe
2almalki <aalmalki22@gmail.com>
2016-02-15 20:20:48 -0500

    integrated taillight and laser flashing

--- integrationGPIO/lookupGPIOinUse.py
+++ integrationGPIO/lookupGPIOinUse.py
@@ -1,10 +1,13 @@
 # import RPi.GPIO module
 import RPi.GPIO as GPIO
+import time
 
 # set pins corresponding to each Zone
 pin_in_lidar_z1 = 24
 pin_in_lidar_z2 = 25
 pin_in_lidar_z3 = 26
+pin_out_LED = 7
+pin_out_LASER = 36
 
 # set GPIO mode
 GPIO.setmode(GPIO.BOARD)
@@ -12,36 +15,52 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin_in_lidar_z1,GPIO.IN)
 GPIO.setup(pin_in_lidar_z2,GPIO.IN)
 GPIO.setup(pin_in_lidar_z3,GPIO.IN)
+# output LED and Laser
+GPIO.setup(pin_out_LED,GPIO.OUT)
+GPIO.setup(pin_out_LASER,GPIO.OUT)
 
-# global variable
-z1 = False
-z2 = False
-z3 = False
+# variables
+flash_speed = 0
+
+# function to find which zone is active
+def closest_zone():
+    # TODO: implement logging instead of print
+    if GPIO.input(pin_in_lidar_z3):
+        # something detected in Z3
+        print "\nZone 3 - detected"
+        return 0.125
+    elif GPIO.input(pin_in_lidar_z2):
+        # nothing detected in Z3, something detected in Z2
+        print "\nZone 2 - detected"
+        return 0.17
+    elif GPIO.input(pin_in_lidar_z1):
+         # nothing detected in Z3,Z2, something detected in Z1
+         print "\nZone 1 - detected"
+         return 0.25
+    else:
+        print "\nNothing detected"
+        return 0
 
 try:
     # main loop
-    # TODO - add taillight / laser lane implementation
     while True:
-        z1 = GPIO.input(pin_in_lidar_z1)
-        z2 = GPIO.input(pin_in_lidar_z2)
-        z3 = GPIO.input(pin_in_lidar_z3)
+        flash_speed = closest_zone()  # update to the most recent zone detectd
 
-        if z3 == True:
-            # something detected in Z3
-            # do something
-            print "\nZone 3 - detected"
-        elif z2 == True:
-            # nothing detected in Z3, something detected in Z2
-            # do something
-            print "\nZone 2 - detected"
-        elif z3 == True:
-            # nothing detected in Z3,Z2, something detected in Z1
-            # do something
-            print "\nZone 1 - detected"
+        # turn on LED / Laser
+        if flash_speed < 0.25: #in z2,3
+            GPIO.output(pin_out_LED, True)
+            GPIO.output(pin_out_LASER,True)
         else:
-            # nothing has been detected
-            print "\nNothing detected"
+            GPIO.output(pin_out_LED, True)
+            GPIO.output(pin_out_LASER,False)
 
+        time.sleep(flash_speed)
+
+        #turn off laser/taillight
+        GPIO.output(pin_out_LED, False)
+        GPIO.output(pin_out_LASER,False)
+
+        time.sleep(flash_speed)
 
 except KeyboardInterrupt:
     # code you want to run before the program
@@ -56,4 +75,4 @@ except KeyboardInterrupt:
 
 finally:
     print "\n Cleanup GPIO"
-    GPIO.cleanup() # this ensures a clean exit
+    GPIO.cleanup() # clean exit
